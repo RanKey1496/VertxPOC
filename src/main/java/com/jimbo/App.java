2dc99189eea9861cc737a5d96220ee061f3981ca
JHON JAIME GIL SEPULVEDA <JGS808@wnutibara.com.co>
2018-10-11 16:51:02 -0500

    First verticle boooooi

---
+++ src/main/java/com/jimbo/App.java
@@ -0,0 +1,24 @@
+package com.jimbo;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+
+public class App extends AbstractVerticle 
+{
+    @Override
+    public void start(Future fut) {
+    	vertx
+    		.createHttpServer()
+    		.requestHandler(r -> {
+    			r.response()
+    				.end("<h1>Hello there.... General Kenobi :v</h1>");
+    		})
+    		.listen(8080, result -> {
+    			if (result.succeeded()) {
+    				fut.complete();
+    			} else {
+    				fut.fail(result.cause());
+    			}
+    		});
+    }
+}
