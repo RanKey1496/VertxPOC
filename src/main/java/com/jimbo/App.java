81f486647d164a5651a860d691a4500cefe6585e
Jhon Gil Sepulveda <jhongil96@gmail.com>
2018-10-12 00:20:29 -0500

    Config from file

--- src/main/java/com/jimbo/App.java
+++ src/main/java/com/jimbo/App.java
@@ -1,5 +1,6 @@
 package com.jimbo;
 
+import io.vertx.config.ConfigRetriever;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
 
@@ -7,18 +8,25 @@ public class App extends AbstractVerticle
 {
     @Override
     public void start(Future fut) {
-    	vertx
-    		.createHttpServer()
-    		.requestHandler(r -> {
-    			r.response()
-    				.end("<h1>Hello there.... General Kenobi :v</h1>");
-    		})
-    		.listen(8080, result -> {
-    			if (result.succeeded()) {
-    				fut.complete();
-    			} else {
-    				fut.fail(result.cause());
-    			}
-    		});
+    	ConfigRetriever retriever = ConfigRetriever.create(vertx);
+    	retriever.getConfig(config -> {
+    		if (config.failed()) {
+    			fut.fail(config.cause());
+    		} else {
+    			vertx
+	        		.createHttpServer()
+	        		.requestHandler(r -> {
+	        			r.response()
+	        				.end("<h1>Hello there.... General Kenobi :v</h1>");
+	        		})
+	        		.listen(config.result().getInteger("HTTP_PORT", 8080), result -> {
+	        			if (result.succeeded()) {
+	        				fut.complete();
+	        			} else {
+	        				fut.fail(result.cause());
+	        			}
+	        		});
+    		}
+    	});
     }
 }
